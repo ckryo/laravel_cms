79bd14e19f4c20de8c77338e91e750788ea13e24
刘荣 <liurong@ckryo.local>
2017-06-04 16:56:35 +0800

    -

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,11 @@
 
 Route::group(['namespace' => 'Ckryo\Laravel\Cms\Controllers', 'prefix' => 'cms'], function ($router) {
 
+
+    $router->get('news', 'NewsController@index');
+    $router->get('notice', 'NoticeController@index');
+
+
     $router->group(['middleware' => 'auth'], function ($router) {
         // 头像上传
         $router->resource('push', 'PushController');
